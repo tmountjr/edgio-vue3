5008469d765e70eaa777a151e5c639933111093f
Tom Mount <tmountjr@gmail.com>
2023-02-22 16:49:09 -0500

    add edgio (no service worker)

---
+++ edgio.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  connector: "@edgio/vue-cva",
+  // The name of the site in Edgio to which this app should be deployed.
+  name: "edgio-vue3",
+};
---
+++ routes.ts
@@ -0,0 +1,10 @@
+// This file was added by edgio init.
+// You should commit this file to source control.
+
+import { Router } from "@edgio/core/router";
+import { vueRoutes } from "@edgio/vue-cva";
+
+// const ONE_HOUR = 60 * 60
+// const ONE_DAY = 24 * ONE_HOUR
+
+export default new Router().use(vueRoutes);

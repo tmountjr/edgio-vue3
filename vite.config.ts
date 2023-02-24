b6569731435e4c3181d6b2aa5e33efec70e0d46e
Tom Mount <tmountjr@gmail.com>
2023-02-24 10:45:13 -0500

    additional cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
-import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
-
-const pwaOptions: Partial<VitePWAOptions> = {
-  devOptions: {
-    enabled: true,
-    type: "module",
-  },
-  srcDir: "sw",
-  filename: "service-worker.js",
-  injectRegister: null,
-  strategies: "injectManifest",
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
